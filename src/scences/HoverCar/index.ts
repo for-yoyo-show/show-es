1e5b06160deb996ab4428c2359663a8fac40f64b
lizj <15014217605@163.com>
2023-07-04 00:23:01 +0800

    oo

--- src/scences/HoverCar/index.ts
+++ src/scences/HoverCar/index.ts
@@ -15,37 +15,68 @@ function startAnimation(skinnedMesh, animations, animationName) {
   return m_mixer;
 }
 
+const addLight = scene => {
+  const intensity = 1;
+  // 创建八个平行光源
+  const light1 = new THREE.DirectionalLight(0xffffff, intensity);
+  const light2 = new THREE.DirectionalLight(0xffffff, intensity);
+  const light3 = new THREE.DirectionalLight(0xffffff, intensity);
+  const light4 = new THREE.DirectionalLight(0xffffff, intensity);
+  const light5 = new THREE.DirectionalLight(0xffffff, intensity);
+  const light6 = new THREE.DirectionalLight(0xffffff, intensity);
+  const light7 = new THREE.DirectionalLight(0xffffff, intensity);
+  const light8 = new THREE.DirectionalLight(0xffffff, intensity);
+
+  // 设置每个平行光源的位置和目标
+  light1.position.set(1, 0, 0);
+  light1.target.position.set(0, 0, 0);
+
+  light2.position.set(-1, 0, 0);
+  light2.target.position.set(0, 0, 0);
+
+  light3.position.set(0, 1, 0);
+  light3.target.position.set(0, 0, 0);
+
+  light4.position.set(0, -1, 0);
+  light4.target.position.set(0, 0, 0);
+
+  light5.position.set(0, 0, 1);
+  light5.target.position.set(0, 0, 0);
+
+  light6.position.set(0, 0, -1);
+  light6.target.position.set(0, 0, 0);
+
+  light7.position.set(1, 1, 1);
+  light7.target.position.set(0, 0, 0);
+
+  light8.position.set(-1, -1, -1);
+  light8.target.position.set(0, 0, 0);
+
+  // 将平行光源添加到场景中
+  scene.add(light1, light2, light3, light4, light5, light6, light7, light8);
+  scene.add(
+    light1.target,
+    light2.target,
+    light3.target,
+    light4.target,
+    light5.target,
+    light6.target,
+    light7.target,
+    light8.target
+  );
+};
+
 const OldBox: Scence = async ({ canvas, loadCallback }) => {
   const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
   const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100);
   const scene = new THREE.Scene();
   const controls = new OrbitControls(camera, renderer.domElement);
+  addLight(scene);
   const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-  const ambientLight2 = new THREE.AmbientLight(0xffffff, 1);
-  const ambientLight3 = new THREE.AmbientLight(0xffffff, 1);
   const directionLight = new THREE.DirectionalLight(0xffffff, 1);
-  directionLight.position.set(0, 100, 0); // 平行光的位置（没有实际意义）
-  directionLight.target.position.set(0, 0, 0); // 平行光的目标位置
-
-  const directionLight2 = new THREE.DirectionalLight(0xffffff, 1);
-  directionLight2.position.set(100, 0, 0); // 平行光的位置（没有实际意义）
-  directionLight2.target.position.set(0, 0, 0); // 平行光的目标位置
-
-  const directionLight3 = new THREE.DirectionalLight(0xffffff, 1);
-  directionLight3.position.set(-100, 0, 0); // 平行光的位置（没有实际意义）
-  directionLight3.target.position.set(0, 0, 0); // 平行光的目标位置
-
-  const directionLight4 = new THREE.DirectionalLight(0xffffff, 1);
-  directionLight4.position.set(100, 100, 100); // 平行光的位置（没有实际意义）
-  directionLight4.target.position.set(0, 0, 0); // 平行光的目标位置
 
   scene.add(ambientLight);
-  scene.add(ambientLight2);
-  scene.add(ambientLight3);
   scene.add(directionLight);
-  scene.add(directionLight2);
-  scene.add(directionLight3);
-  scene.add(directionLight4);
   scene.background = new THREE.Color(0.2, 0.2, 0.2);
   renderer.outputColorSpace = THREE.SRGBColorSpace;
   renderer.setClearColor(new THREE.Color(0x000000));
@@ -54,6 +85,8 @@ const OldBox: Scence = async ({ canvas, loadCallback }) => {
 
   let mixer;
   let keyDownEvent;
+  let touchStartEvent;
+  let touchEndEvent;
 
   const loader = new GLTFLoader().setPath('models/glb/hovercar/');
   loader.load('scene.gltf', gltf => {
@@ -82,9 +115,16 @@ const OldBox: Scence = async ({ canvas, loadCallback }) => {
         mixer?.stopAllAction();
       }
     };
+    touchStartEvent = () => {
+      mixer = startAnimation(root, animations, 'Take 01');
+    };
+    touchEndEvent = () => {
+      mixer?.stopAllAction();
+    };
 
     loadCallback();
-
+    window.addEventListener('touchstart', touchStartEvent);
+    window.addEventListener('touchend', touchEndEvent);
     window.addEventListener('keydown', keyDownEvent);
   });
   const state = {
@@ -117,6 +157,8 @@ const OldBox: Scence = async ({ canvas, loadCallback }) => {
   const finish = () => {
     state.running = false;
     window.removeEventListener('keydown', keyDownEvent);
+    window.removeEventListener('touchstart', touchStartEvent);
+    window.removeEventListener('touchend', touchEndEvent);
   };
 
   return {
